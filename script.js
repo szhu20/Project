47b78e98d19a1340775a9bd9304000c99e8aa26e
Sebastian Zhu <sebastian1.zhu@gmail.com>
2018-05-29 00:08:14 -0400

    Done!

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 var colors = ["red ball", "blue ball", "yellow ball", "green ball"];
 var current = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 // changes bead color when clicked on
-function click(num, id) {
+function onClick(num, id) {
   current[num] = (current[num] + 1) % 4;
-  document.getElementById(id).className = "blue ball";
-  var same = false;
-  var i;
-  for (i = 0; i < 8; i++) {
-
+  document.getElementById(id).className = "bead" + (num % 8 + 1) + " " + colors[current[num]] + " ball";
+  var same;
+  for (var i = 0; i < 8; i++) {
+    same = true;
+    for (var j = 0; j < 8; j++) {
+      if (current[j] != current[(i + j) % 8 + 8]) {
+        same = false;
+        break;
+      };
+    };
+    if (same) {
+      document.getElementById("light").className = "green ball";
+      break;
+    };
   };
+  if (!same) {
+    document.getElementById("light").className = "red ball";
+  }
 };
